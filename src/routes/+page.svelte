a17be70379b2a7d5db5117dd4f4ffdee6a59efaa
Wedkwan <wedneykawan5@gmail.com>
2024-12-23 13:37:29 -0300

    add nave e nome

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <div class = "cenario">
     <img src="/src/static/jupt.png" alt="jutp" class="jupt">
-    
-    <div class="itens ">
+     <img src="/src/static/space.png" alt="" class= "space">
+    <img src="/src/static/nave.png" alt="" class = "nave">
+    <div class="it ">
 
         <a href="/jogar" id= "jogar">jogar</a>
-        <a href="" id= "sobre">sobre</a>
+        <a href="/jogar" id= "sobre">sobre</a>
     </div>
 
 
