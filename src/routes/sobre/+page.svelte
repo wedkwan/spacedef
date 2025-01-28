db71ae40c67e303a907ab18218aee89d1154aeb6
Wedkwan <wedneykawan5@gmail.com>
2025-01-27 22:35:12 -0300

    add sobre e novo jup

---
+++ src/routes/sobre/+page.svelte
@@ -0,0 +1,13 @@
+<div class="sobre-container">
+    <img class="sobre-logo" src="src/static/logif.jpg" alt="Logo IFPE" />
+    
+    <p class="sobre-text">
+      Autor: Jean Lucas, Paulo, Wedney
+    </p>
+    <p class="sobre-text">
+      <a href="mailto:allan.lima@igarassu.ifpe.edu.br">allan.lima@igarassu.ifpe.edu.br</a>
+    </p>
+    
+    <a class="menu" href="/">BACK</a>
+  </div>
+  
