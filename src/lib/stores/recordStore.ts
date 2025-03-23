73693de4d6d6ebfdac26d9db4499de93ed3404ee
Wedkwan <wedneykawan5@gmail.com>
2025-03-23 01:47:42 -0300

    add2

--- src/lib/stores/recordStore.ts
+++ src/lib/stores/recordStore.ts
@@ -1,4 +1,5 @@
 import { writable } from "svelte/store";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 export type Recorde = {
   nick: string;
@@ -9,7 +10,7 @@ export const recorde = writable<Recorde>({ nick: "Desconhecido", score: 0 });
 
 export async function carregarRecorde() {
   try {
-    const response = await fetch("http://localhost:3000/records");
+    const response = await fetch(`${apiUrl}/records`);
     if (!response.ok) {
       throw new Error("Erro ao buscar recorde.");
     }
