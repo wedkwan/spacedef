42e8c357bc0acd1957249b137f97ac851a1d0c38
Wedkwan <wedneykawan5@gmail.com>
2025-03-22 23:45:44 -0300

    dep

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { sveltekit } from "@sveltejs/kit/vite";
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	resolve: {
-		extensions : ['.mjs', '.js' , '.ts' , '.svelte']
-	}
+  plugins: [sveltekit()],
+  resolve: {
+    extensions: [".mjs", ".js", ".ts", ".svelte"],
+  },
+  server: {
+    host: "0.0.0.0", // Permite acesso externo
+    port: 8080, // Usa a porta esperada pelo Railway
+    strictPort: true,
+    allowedHosts: ["invigorating-vitality-production.up.railway.app"], // Adiciona seu domínio
+  },
 });
